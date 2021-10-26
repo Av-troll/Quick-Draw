4399576e85ddd034d8d0700c5fbe0d55589641e8
AvniBookworm4ever <79648896+Av-troll@users.noreply.github.com>
2021-10-26 16:40:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(580,580);
+    canvas.center();
+    background('white');
+}
+ function ClearCanvas(){
+     background('white');
+ }
